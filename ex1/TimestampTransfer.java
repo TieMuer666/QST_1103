e1defaed7592a2ec926c7a1b2b1eae56074d3a55
TieMuer666 <353650331@qq.com>
2016-11-03 14:40:16 +0800

    Update TimestampTransfer.java

--- ex1/TimestampTransfer.java
+++ ex1/TimestampTransfer.java
@@ -14,20 +14,20 @@ import java.util.Scanner;
  * 3. 创建pull request，与主仓库的master分支对比
  */
 public class TimestampTransfer {
-	@SuppressWarnings("resource")
+	@SuppressWarnings("resource")	//告诉编译器忽略指定的警告，不用在编译完成后出现警告信息。
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			Date lineDate = null;
-			long lineTimestamp;
+		Scanner scanner = new Scanner(System.in);	//新创建一个输入的Scanner 对象，然后赋值给in。
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");	//对日期字符串进行解析和格式化输入
+		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");	//对日期字符串进行解析和格式化输出
+		while (scanner.hasNext()){	//要判断scanner当前是否有下个节点 有才执行
+			String line = scanner.nextLine();	//使用nextLine方式接收字符串，赋值给line
+			Date lineDate = null;	//初始化lineDate，类型为Date
+			long lineTimestamp;	//定义lineTimestamp，为long类型
 			try {
-				lineDate = inputFormat.parse(line);
-				lineTimestamp = lineDate.getTime();
-				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);
-			} catch (ParseException e) {
+				lineDate = inputFormat.parse(line);	//将line的值赋给lineDate	
+				lineTimestamp = lineDate.getTime();	//使用getTime()方法得到时间戳
+				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);	//输出合并后的：日期和时间戳
+			} catch (ParseException e) {	//异常捕捉	
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
